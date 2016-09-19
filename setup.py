a64ba1be8addab398ff86c7f42a33ca218e6516d
Tommaso <cemtomma@gmail.com>
2016-09-20 01:24:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ except ImportError:
 setup(
       name='CSVrope',
       version='1.0.0',
-	  license='Creative Commons Attribution 4.0 International license',
+      license='Creative Commons Attribution 4.0 International license',
       author='thumbo',
+      url='https://github.com/thumbo/CSVrope',
       install_requires=['nose'],
       packages=['csvrope'],
       description='A set of row-operations for CSV files.',
